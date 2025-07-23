c2f89c48d1d853c21fcb3aa47cec1006dd54452d
Mingi Hwang <hwang.mingi7@gmail.com>
2025-07-23 16:40:06 +0900

    Avoid one level of indentation

--- src/MatrixScanner.h
+++ src/MatrixScanner.h
@@ -44,78 +44,79 @@ void scan_keys() {
         current_adc_reading = (spi_read() << 1) | (spi_read() == 0x80);
         adc_values[idx] = current_adc_reading;
         spi_deselect_slave();
+        
+        // Only execute rapid trigger part when enabled
+        if (!RAPID_TRIGGER_ENABLED) continue;
 
-        if (RAPID_TRIGGER_ENABLED) {
-            // Rapid Trigger Enabled: Right LED
-            PORTD &= ~(1 << PD5);
+        // Rapid Trigger Enabled: Right LED
+        PORTD &= ~(1 << PD5);
 
-            // Rapid Trigger!
-            // If key is idle, skip and move to next key
-            if (adc_values[idx] == old_values[idx]) {
-                key_idle_for[idx]++;
-                if (key_actions[idx] != IDLE \
-                        && key_idle_for[idx] >= RAPID_TRIGGER_IDLE_HYSTERESIS) {
-                    key_idle_for[idx] = 0;
-                    key_actions[idx] = IDLE;
-                    adc_maxima[idx] = current_adc_reading;
-                    adc_minima[idx] = current_adc_reading;
-                }
-            }
-
-            // If key is being pressed, record it
-            else if (adc_values[idx] < old_values[idx]) {
+        // Rapid Trigger!
+        // If key is idle, skip and move to next key
+        if (adc_values[idx] == old_values[idx]) {
+            key_idle_for[idx]++;
+            if (key_actions[idx] != IDLE \
+                    && key_idle_for[idx] >= RAPID_TRIGGER_IDLE_HYSTERESIS) {
                 key_idle_for[idx] = 0;
-                if (key_being_pressed_for[idx] == 0) {
-                    candidate_maxima[idx] = old_values[idx];
-                }
-                key_being_pressed_for[idx]++;
-                // If key is being pressed just now, record previous value as max
-                // to compare against rapid trigger threshold
-                if (key_actions[idx] != BEING_PRESSED \
-                    && (key_being_pressed_for[idx] >= RAPID_TRIGGER_DIRECTION_HYSTERESIS \
-                        || old_values[idx] - adc_values[idx] > RAPID_TRIGGER_SHORT_CIRCUIT_THRESHOLD)) {
-                    key_being_released_for[idx] = 0;
-                    key_actions[idx] = BEING_PRESSED;
-                    adc_maxima[idx] = candidate_maxima[idx];
-                }
-                // If key was already being pressed, send press keystroke if
-                // delta(current, highest) >= threshold
-                if (adc_maxima[idx] - current_adc_reading >= RAPID_TRIGGER_THRESHOLD) {
-                    key_status[idx] = PRESSED;
-                }
-            }
-
-            // If key is being released, record it
-            else {
-                key_idle_for[idx] = 0;
-                if (key_being_released_for[idx] == 0) {
-                    candidate_minima[idx] = old_values[idx];
-                }
-                key_being_released_for[idx]++;
-                // If key is being released just now, record previous value as min
-                // to compare against rapid trigger threshold
-                if (key_actions[idx] != BEING_RELEASED \
-                    && (key_being_released_for[idx] >= RAPID_TRIGGER_DIRECTION_HYSTERESIS \
-                        || adc_values[idx] - old_values[idx] > RAPID_TRIGGER_SHORT_CIRCUIT_THRESHOLD)) {
-                    key_being_pressed_for[idx] = 0;
-                    key_actions[idx] = BEING_RELEASED;
-                    adc_minima[idx] = candidate_minima[idx];
-                }
-                // If key was already being released, send release keystroke if
-                // delta(current, lowest) >= threshold
-                if (current_adc_reading - adc_minima[idx] >= RAPID_TRIGGER_THRESHOLD) {
-                    key_status[idx] = RELEASED;
-                }
-            }
-
-            // Finally, as hacky workaround to release "stuck" keys
-            if (current_adc_reading >= ADC_BASELINE[idx] - RAPID_TRIGGER_THRESHOLD) {
+                key_actions[idx] = IDLE;
                 adc_maxima[idx] = current_adc_reading;
+                adc_minima[idx] = current_adc_reading;
+            }
+        }
+
+        // If key is being pressed, record it
+        else if (adc_values[idx] < old_values[idx]) {
+            key_idle_for[idx] = 0;
+            if (key_being_pressed_for[idx] == 0) {
+                candidate_maxima[idx] = old_values[idx];
+            }
+            key_being_pressed_for[idx]++;
+            // If key is being pressed just now, record previous value as max
+            // to compare against rapid trigger threshold
+            if (key_actions[idx] != BEING_PRESSED \
+                && (key_being_pressed_for[idx] >= RAPID_TRIGGER_DIRECTION_HYSTERESIS \
+                    || old_values[idx] - adc_values[idx] > RAPID_TRIGGER_SHORT_CIRCUIT_THRESHOLD)) {
+                key_being_released_for[idx] = 0;
+                key_actions[idx] = BEING_PRESSED;
+                adc_maxima[idx] = candidate_maxima[idx];
+            }
+            // If key was already being pressed, send press keystroke if
+            // delta(current, highest) >= threshold
+            if (adc_maxima[idx] - current_adc_reading >= RAPID_TRIGGER_THRESHOLD) {
+                key_status[idx] = PRESSED;
+            }
+        }
+
+        // If key is being released, record it
+        else {
+            key_idle_for[idx] = 0;
+            if (key_being_released_for[idx] == 0) {
+                candidate_minima[idx] = old_values[idx];
+            }
+            key_being_released_for[idx]++;
+            // If key is being released just now, record previous value as min
+            // to compare against rapid trigger threshold
+            if (key_actions[idx] != BEING_RELEASED \
+                && (key_being_released_for[idx] >= RAPID_TRIGGER_DIRECTION_HYSTERESIS \
+                    || adc_values[idx] - old_values[idx] > RAPID_TRIGGER_SHORT_CIRCUIT_THRESHOLD)) {
+                key_being_pressed_for[idx] = 0;
+                key_actions[idx] = BEING_RELEASED;
+                adc_minima[idx] = candidate_minima[idx];
+            }
+            // If key was already being released, send release keystroke if
+            // delta(current, lowest) >= threshold
+            if (current_adc_reading - adc_minima[idx] >= RAPID_TRIGGER_THRESHOLD) {
                 key_status[idx] = RELEASED;
             }
-            // Rapid Trigger Enabled: Right LED
-            PORTD |= (1 << PD5);
         }
+
+        // Finally, as hacky workaround to release "stuck" keys
+        if (current_adc_reading >= ADC_BASELINE[idx] - RAPID_TRIGGER_THRESHOLD) {
+            adc_maxima[idx] = current_adc_reading;
+            key_status[idx] = RELEASED;
+        }
+        // Rapid Trigger Enabled: Right LED
+        PORTD |= (1 << PD5);
     }
 }
 
