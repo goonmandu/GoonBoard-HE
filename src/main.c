b883914f8945a77a25728b822eebcc7afa4739d2
Mingi Hwang <hwang.mingi7@gmail.com>
2025-08-14 15:12:21 +0900

    Remove unnecessary NKRO buffer declaration

--- src/main.c
+++ src/main.c
@@ -8,7 +8,6 @@
 extern uint8_t PrevKeyboardHIDReportBuffer[PREV_REPORT_BUFFER_SIZE];
 extern USB_ClassInfo_HID_Device_t Keyboard_HID_Interface;
 extern USB_ClassInfo_HID_Device_t RawHID_HID_Interface;
-extern USB_ClassInfo_HID_Device_t NKRO_Keyboard_HID_Interface;
 
 int main(void) {
     SetupHardware();
